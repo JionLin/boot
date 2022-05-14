9dc62174ba52cdc77012e51f118c95a535bb23fb
JionLin <250912986@qq.com>
2022-05-14 20:45:58 +0800

    1. 指定配置文件激活有3种方式, 第一:使用配置文件,第二:在虚拟机里面进行配置 -Dspring.profiles.active=dev 第三:打成jar包,进行输入命令 java -jar boot --spring.profiles.active=dev

--- src/main/java/com/jiaolin/boot/BootApplication.java
+++ src/main/java/com/jiaolin/boot/BootApplication.java
@@ -3,6 +3,12 @@ package com.jiaolin.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 指定配置文件激活有3种方式,
+ * 第一:使用配置文件,
+ * 第二:在虚拟机里面进行配置 -Dspring.profiles.active=dev
+ * 第三:打成jar包,进行输入命令 java -jar boot --spring.profiles.active=dev
+ */
 @SpringBootApplication
 public class BootApplication {
 
